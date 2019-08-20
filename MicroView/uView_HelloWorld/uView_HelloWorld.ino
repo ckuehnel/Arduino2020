c0adf35d256957b21a612b6460f8ba0064b6879c
Claus Kühnel <info@ckuehnel.ch>
2019-08-20 15:35:06 +0200

    Add files via upload

--- MicroView/uView_HelloWorld/uView_HelloWorld.ino
+++ MicroView/uView_HelloWorld/uView_HelloWorld.ino
@@ -1,1 +1,17 @@
+#include <MicroView.h>
 
+#define BOARD "MicroView"
+
+void setup() {
+	uView.begin();				      // start MicroView
+	uView.clear(PAGE);			    // clear page
+	uView.print("HelloWorld");	// display HelloWorld
+  uView.print("This is\n");
+  uView.println(BOARD);
+  uView.print("Clock\n ");
+  uView.print(F_CPU/1000000);      // Oscillator frequency
+  uView.print(" MHz");
+	uView.display();
+}
+
+void loop () {}
