52bf0f8775bc0fd797189c74ca4e9601b3118b9e
Claus Kühnel <info@ckuehnel.ch>
2020-01-14 17:21:23 +0100

    Add files via upload

--- Arduino Uno/ArduinoUnoTest/ArduinoUnoTest.ino
+++ Arduino Uno/ArduinoUnoTest/ArduinoUnoTest.ino
@@ -1,1 +1,29 @@
+/*
+  Arduino Uno Test
+  (c) Claus Kuehnel 2019-10-12 info@ckuehnel.ch
+*/
 
+byte count = 0;
+
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  delay(1000); // waiting to start the monitor
+  Serial.print("Hi there, here is Arduino Uno"); 
+}
+
+void loop() 
+{
+  Serial.print(".");
+  count++;
+  if (count == 25)
+  {
+    Serial.println();
+    count=0;
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
