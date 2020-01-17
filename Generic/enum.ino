5f73d98b88ef3315001749fe377c90b5075e0549
Claus Kühnel <info@ckuehnel.ch>
2020-01-17 16:45:50 +0100

    Add files via upload

---
+++ Generic/enum.ino
@@ -0,0 +1,16 @@
+enum Days{Sun,Mon,tue,Wed,thu,Fri,Sat};
+enum Colors{Red=10, Green, Blue};
+
+void setup() 
+{
+	Serial.begin(115200);
+	
+	Serial.println(Mon);
+	Serial.println(Fri);
+	Serial.println(Green);
+	
+	Colors myFavorite = Red;
+	Serial.println(myFavorite);
+}
+
+void loop() {}
