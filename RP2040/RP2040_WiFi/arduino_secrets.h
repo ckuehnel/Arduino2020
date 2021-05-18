f9f99eee78af7b14f4486e8acb6cfb76ac9ba0e3
Claus Kühnel <info@ckuehnel.ch>
2021-05-18 16:46:44 +0200

    Update arduino_secrets.h

--- RP2040/RP2040_WiFi/arduino_secrets.h
+++ RP2040/RP2040_WiFi/arduino_secrets.h
@@ -1,10 +1,10 @@
 // Access data for services
 
 // WiFi
-const char* ssid   = "Sunrise_2.4GHz_8AC2A0";
-const char* pass   = "u2u7fgzv31Ds";
+const char* ssid   = <Your SSID>;
+const char* pass   = <Your Password>;
 
 // Thingspeak
-// GET https://api.thingspeak.com/update?api_key=33RVWP4KTWOOBSZE&field1=0&field2=0
+// GET https://api.thingspeak.com/update?api_key=33xxxxxxxxxxxxxxxxZE&field1=0&field2=0
 const char* server = "api.thingspeak.com";
-String api_key     = "33RVWP4KTWOOBSZE";
+String api_key     = <Your Write API Token>;
