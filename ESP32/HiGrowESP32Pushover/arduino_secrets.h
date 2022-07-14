61a1105f473bf4f20e307a53ab104c15a9c41589
Claus Kühnel <info@ckuehnel.ch>
2022-07-14 12:43:34 +0200

    File Upload

---
+++ ESP32/HiGrowESP32Pushover/arduino_secrets.h
@@ -0,0 +1,10 @@
+// This file contains credentials to access several services
+
+// WiFi
+const char* SSID = "<Your SSID>";
+const char* PASS = "<Your Password>";
+
+// Pushover settings
+char pushoversite[] = "api.pushover.net";
+char apitoken[] = "<Your API Token>";
+char userkey [] = "<Your User Key>";
